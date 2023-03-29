fd75a01d94f2d9e2330443fa07477b83f44a5d6f
Dom231 <114959233+Dom231@users.noreply.github.com>
2023-03-28 19:07:59 -0500

    update1

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,7 @@ type DayMealPlan {
 type Profile {
     _id: ID
     username: String
+    password: String
     email: String
     calorie_goal: Int
     body_weight: Int
@@ -48,12 +49,15 @@ type Auth {
 
 type Query{ 
     me : Profile
+    meals: Meal
+    meal(_id): Meal
+
 }
 
 type Mutation {
     login(email: String!, password: String): Auth
     addProfile(username: String!, email: String!, password: String!): Auth
-        // addDayPlan(title: String!, breakfast: Meal!, lunch: Meal!, Dinner: Meal!): Profile
+    // addDayPlan(title: String!, breakfast: Meal!, lunch: Meal!, Dinner: Meal!): Profile
     // addMeal(title: String!, calories: Int!, fat: Int!, protein: Int!, carbs: Int!, image: String! ):Profile
     // removeDayPlan(_id: ID :Profile
     // removeMeal(api_id: Int!):Profile
