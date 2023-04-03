cb3de37fea09d688de657d4567a6dfac411eb547
kavic <kavicse.23@gmail.com>
2023-04-03 07:43:18 -0500

    fixing conflict

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer style={{backgroundColor: 'black'}}>
       <Container style={{height: '6rem', fontSize: 30, color: 'white'}}>
         <Row>
-          <Col className='text-center py-3'>
+          <Col className='text-center '>
           Copyright &copy; Fitness meal planner</Col>
         </Row>
       </Container>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,108 +1,110 @@
-// import React from 'react';
-// // import { FaBars, FaTimes } from 'react-icons/fa';
-// // import {IconContext } from 'react-icons/lib';
-// import { Nav,NavbarContainer ,NavLogo, NavMenu, NavItem, NavLink } from './IndexElement';
-// // import {Button} from '../../globalStyles';
-
-// const Navbar = () => {
-
-// // const [click, setClick] = useState(false);
-
-// // const handleClick = () => setClick(!click);
-//   return (
-//     <>
-   
-//       <Nav>
-// <NavbarContainer >
-//   <NavLogo to='/'>
-//     Fitness Meal Planner
-//   </NavLogo>
-//   {/* <HamburgerIcon onClick={handleClick}>
-//     {click ? <FaTimes /> : <FaBars />}
-//   </HamburgerIcon> */}
-//   </NavbarContainer>
-
-// <NavMenu >
-//   <NavItem>
-//     <NavLink to='/' >
-//       Home
-//     </NavLink>
-//   </NavItem>
-
-//   <NavItem>
-//     <NavLink to='/meallist' >
-//       Meal Plan
-//     </NavLink>
-//   </NavItem>
-
-//   <NavItem>
-//     <NavLink to='/me' >
-//       Profile
-//     </NavLink>
-//   </NavItem>
-
-//   <NavItem>
-//     <NavLink to='/login' >
-//     Login
-//     </NavLink>
-//   </NavItem>
-
-//   <NavItem>
-//     <NavLink to='/aboutus' >
-//       About Us
-//     </NavLink>
-//   </NavItem>
-
-// </NavMenu>
-
-//       </Nav>
-    
-//     </>
-//   )
-// }
-
-// export default Navbar
-
-import React from 'react'
+import React, {useState}from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { useRef } from 'react';
-import './navbar.css';
-import { Link } from 'react-router-dom';
+// import {IconContext } from 'react-icons/lib';
+import { Nav,NavbarContainer ,NavLogo, NavMenu, NavItem, NavLink,HamburgerIcon, NavBtn, NavBtnLink } from './indexElement';
+// import {Button} from '../../globalStyles';
 
-function Navbar() {
+const Navbar = () => {
 
-const navRef = useRef();
+ const [click, setClick] = useState(false);
 
-const showNavbar = () => {
-  navRef.current.classList.toggle("responsive_nav");
-}
-
-
-
+ const handleClick = () => setClick(!click);
   return (
-    <div>
-      <header>
-        <h1 ><Link className='title' to='/'>Fitness Meal Planner</Link></h1>
-        <nav  ref={navRef}>
-          <Link to="/">Home</Link>
-          <Link to="/meallist">Meal Plan</Link>
-          <Link to="/me">Profile</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/aboutus">About Us</Link>
+    <>
+   
+      <Nav>
+<NavbarContainer >
+  <NavLogo to='/'>
+    Fitness Meal Planner
+  </NavLogo>
 
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-<FaTimes />
+  <HamburgerIcon onClick={handleClick}>
+    {click ? <FaTimes /> : <FaBars />}
+  </HamburgerIcon>
+  <NavMenu >
+  <NavItem>
+    <NavLink to='/' >
+      Home
+    </NavLink>
+  </NavItem>
 
-          </button>
-        </nav>
-        <button className="nav-btn" onClick={showNavbar}> 
-          <FaBars />
-        </button>
-      </header>
+  <NavItem>
+    <NavLink to='/meallist' >
+      Meal Plan
+    </NavLink>
+  </NavItem>
 
-      
-    </div>
+  <NavItem>
+    <NavLink to='/me' >
+      Profile
+    </NavLink>
+  </NavItem>
+
+  <NavItem>
+    <NavLink to='/login' >
+    Login
+    </NavLink>
+  </NavItem>
+
+  <NavItem>
+    <NavLink to='/aboutus' >
+      About Us
+    </NavLink>
+  </NavItem>
+
+</NavMenu>
+<NavBtn>
+  <NavBtnLink to='/login'>Sign In</NavBtnLink>
+</NavBtn>
+  </NavbarContainer>
+      </Nav>
+    
+    </>
   )
 }
 
-export default Navbar;
+export default Navbar
+
+// import React from 'react'
+// import { FaBars, FaTimes } from 'react-icons/fa';
+// import { useRef } from 'react';
+// import './navbar.css';
+// import { Link } from 'react-router-dom';
+
+// function Navbar() {
+
+// const navRef = useRef();
+
+// const showNavbar = () => {
+//   navRef.current.classList.toggle("responsive_nav");
+// }
+
+
+
+//   return (
+//     <div>
+//       <header>
+//         <h1 ><Link className='title' to='/'>Fitness Meal Planner</Link></h1>
+//         <nav  ref={navRef}>
+//           <Link to="/">Home</Link>
+//           <Link to="/meallist">Meal Plan</Link>
+//           <Link to="/me">Profile</Link>
+//           <Link to="/login">Login</Link>
+//           <Link to="/aboutus">About Us</Link>
+
+//           <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+// <FaTimes />
+
+//           </button>
+//         </nav>
+//         <button className="nav-btn" onClick={showNavbar}> 
+//           <FaBars />
+//         </button>
+//       </header>
+
+      
+//     </div>
+//   )
+// }
+
+// export default Navbar;
